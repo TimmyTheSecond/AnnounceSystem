40cb5dca63da08ad1aeb0cbe355f3d0b253333e4
Timmy <tommy.kinghorn10@gmail.com>
2026-05-09 23:23:26 +1000

    Update index.js

--- index.js
+++ index.js
@@ -836,7 +836,7 @@ const drawScene = () => {
 			context.fill();
 
 			context.strokeStyle = isHovered ? "white" : "black";
-			context.lineWidth = Math.max((isHovered ? 0.7 : 0.4) * scaleFactor, 0.25);
+			context.lineWidth = 1
 			context.stroke();
 		}
 	});
